4c96927f490087c21f4ffa1d6c7af00100b64d73
ArsenSharipov <arsen.sharipov.92@list.ru>
2024-09-04 12:43:42 +0500

    conflict resolved

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@main.route("/")
+@app.route("/")
 def home_page():
     return render_template('gradient.html')
 if __name__ == '__main__':
